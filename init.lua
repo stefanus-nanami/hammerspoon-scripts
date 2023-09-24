584a756d527b498004b0f331fcb6b505d2510924
Stefanus Nanami <saint_nus@mac.com>
2023-09-25 00:06:29 +0900

    Minor changes.

--- init.lua
+++ init.lua
@@ -3,5 +3,6 @@
 -- Center focused window.
 hs.hotkey.bind({ "cmd", "alt", "shift", "ctrl" }, "F12", function()
     local scr = hs.screen.primaryScreen()
-    local w = hs.window.focusedWindow():centerOnScreen(scr, true)
+    local w = hs.window.focusedWindow()
+    w:centerOnScreen(scr, true)
 end)
