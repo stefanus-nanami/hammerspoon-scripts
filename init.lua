3223130badce23530f9c462d0d6ee4971c41c198
Stefanus Nanami <stefanus-nanami@capcom.com>
2023-12-25 10:04:01 +0900

    No offset for snapping.

--- init.lua
+++ init.lua
@@ -4,7 +4,7 @@
 local window_op = require "windowOperations"
 
 window_op:centerWindowHotKey()
-window_op:snapTopLeft(5, 5)
-window_op:snapTopRight(5, 5)
-window_op:snapBottomLeft(5, 5)
-window_op:snapBottomRight(5, 5)
+window_op:snapTopLeft(0, 0)
+window_op:snapTopRight(0, 0)
+window_op:snapBottomLeft(0, 0)
+window_op:snapBottomRight(0, 0)
